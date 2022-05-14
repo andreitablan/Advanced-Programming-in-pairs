5b1d00e27be2b2e7876bd4fbe032aebecbfbaead
Leagan-Dan <leaganndanut@gmail.com>
2022-05-14 19:22:42 +0300

    created lab 11 compulsory

---
+++ Laborator 11/Compulsory/compulsory/src/main/java/compulsory/Person.java
@@ -0,0 +1,45 @@
+package compulsory;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="person")
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    @Column(name="name")
+    private String name;
+
+    public Person() {
+    }
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
