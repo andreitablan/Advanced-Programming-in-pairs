d588fd903d9cbda0de163e0ab1759e3e359b3a49
Andrie Tablan <andreitablan01@gmail.com>
2022-05-23 15:34:22 +0300

    modificari

--- Laborator 12/compulsory1/src/main/java/compulsory/Main.java
+++ Laborator 12/compulsory1/src/main/java/compulsory/Main.java
@@ -1,4 +1,12 @@
 package compulsory;
 
+import java.util.Random;
+
 public class Main {
+    public static void main(String args[]) {
+        MyClass object = new MyClass(); //→ right here...
+        Integer number = new Integer(0); //→ java.lang
+        Random random = new Random(); //→ java.util
+        PDDocument doc = new PDDocument();//→ org.apache.pdfbox.pdmodel
+    }
 }
