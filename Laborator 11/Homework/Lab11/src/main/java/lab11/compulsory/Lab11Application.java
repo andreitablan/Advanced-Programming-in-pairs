176740011b837b2fb53bf17e4924d0279aaf3fd2
Leagan-Dan <leaganndanut@gmail.com>
2022-05-23 15:10:44 +0300

    Create Swagger Documentation

--- Laborator 11/Homework/Lab11/src/main/java/lab11/compulsory/Lab11Application.java
+++ Laborator 11/Homework/Lab11/src/main/java/lab11/compulsory/Lab11Application.java
@@ -2,8 +2,10 @@ package lab11.compulsory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class Lab11Application {
 
 	public static void main(String[] args) {
