d1ab801fb79bad5e322606d79e0e2565857470ba
Andrie Tablan <andreitablan01@gmail.com>
2022-05-24 21:15:45 +0300

    .

--- Laborator 12/Homework/src/main/java/main/Main.java
+++ Laborator 12/Homework/src/main/java/main/Main.java
@@ -1,7 +1,8 @@
 package main;
 
 public class Main {
-    public static void main(String[] args) {
-
-    }
+        public static void main (String args[]){
+            ClassHandler classHandler = new ClassHandler();
+            classHandler.run();
+        }
 }
